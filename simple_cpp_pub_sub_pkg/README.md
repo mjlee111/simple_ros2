b91f627834fb8573d506f182086a9b67c9c1d380
Myeung Jin Lee <menggu1234@naver.com>
2023-08-05 03:19:34 +0900

    pub sub added

---
+++ simple_cpp_pub_sub_pkg/README.md
@@ -0,0 +1,2 @@
+# simple_cpp_pub_sub_pkg
+Simple ROS2 package to publish and subscribe "Hello World!"
---
+++ simple_cpp_pub_sub_pkg/src/simple_cpp_subscriber.cpp
@@ -0,0 +1,35 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+
+class HelloworldSubscriber : public rclcpp::Node
+{
+    public:
+    HelloworldSubscriber()
+    : Node("helloworld_subscriber")
+    {
+        subscription_ = this->create_subscription<std_msgs::msg::String>("topic", 10, std::bind(&HelloworldSubscriber::topic_callback, this, _1));
+    }
+
+    private:
+        void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+        {
+            RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+        }
+
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<HelloworldSubscriber>());
+    rclcpp::shutdown();
+    return 0;
+}
